9d9a4e300506ea3d1bf4e4ba49e89ec110f6f23f
LKW <595282775@qq.com>
2018-04-24 22:30:42 +0800

    第一个提交

--- Finance/ViewController.swift
+++ Finance/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let label = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: 100, height: 100));
+        self.view.addSubview(label);
+        
     }
 
     override func didReceiveMemoryWarning() {
